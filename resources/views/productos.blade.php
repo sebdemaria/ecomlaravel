4fdf6b5e014403d866bd8d94dc637b019ab2ae35
sebademaria <sebdemaria@hotmail.com>
2019-12-13 16:18:37 -0300

    arreglo css

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -9,37 +9,22 @@
 <!-- MAIN -->
 @section('main')
   <div class="caja-principal">
+    <div class="paginacion">
+      {{$productos->links()}}
+    </div>
     @foreach ($productos as $producto)
       <div class="productos">
-
-        <!-- Nombre del producto -->
-        <p>
-          Nombre: {{$producto->nombre}}
-        </p>
 
         <!-- Imagen del producto -->
         <p>
           <img src="/storage/samsung-a50.png" alt="foto-producto">
         </p>
 
-        <!-- Botones -->
-        <div class="botones">
+        <!-- Nombre del producto -->
+        <p>
+          Nombre: {{$producto->nombre}}
+        </p>
 
-          <!-- Compra -->
-          <div class="button">
-            <a href="checkout"><button id="button-compra" type="button-compra" class="btn btn-primary">Comprar</button><a>
-          </div>
-
-          <!--Agregar al carrito-->
-          <div class="button">
-            <a href="carrito"><button id="button-agregar" type="button" class="btn btn-outline-primary">Agregar al carrito</button>
-          </div>
-
-          <!--Ver más -->
-          <div class="button">
-            <a href="detalle-prod"><button id="button-agregar" type="button" class="btn btn-outline-primary">Ver más</button></a>
-          </div>
-        </div>
 
         <!-- Marca del producto -->
         <p>
@@ -50,6 +35,11 @@
         <p>
           Precio: ${{$producto->precio}}
         </p>
+          <!--Ver más -->
+          <div class="button">
+            <a href="detalle-prod"><button id="button-agregar" type="button" class="btn btn-outline-primary">Ver más</button></a>
+          </div>
+
       </div>
     @endforeach
   </div>
