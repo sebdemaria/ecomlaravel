836aa4ef4a69cf499f70ccd3e78b476b1315176e
sebademaria <sebdemaria@hotmail.com>
2019-12-18 17:26:48 -0300

    corrijo css

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -95,12 +95,9 @@
       </div>
     </form>
   </div>
-<<<<<<< HEAD
     <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
     <!--<script src="/js/register_scripts.js"></script>-->
     <script src="/js/register_scripts.js"></script>
-=======
   <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
   <script src="/js/register_scripts.js"></script>
->>>>>>> 6e55955f04da011ecd4304f3d8924be7cf74c3dc
 @endsection
